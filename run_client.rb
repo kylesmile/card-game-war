92d8119f64d938a16af6f52fc4d6f0ed5d5081ce
Kyle Smith <kyle@smithsrus.com>
2013-12-13 15:53:12 -0500

    Remove card rigging in run_server.rb

--- run_client.rb
+++ run_client.rb
@@ -1,5 +1,8 @@
 require_relative './war_game_client'
 
+puts "Press enter to play a card"
+puts "The round results will be displayed after both players have played"
+
 client = WarGameClient.new
 
 loop do
--- run_server.rb
+++ run_server.rb
@@ -1,25 +1,8 @@
 require_relative './war_game_server'
 
-class WarGame # Cheat to make testing feasible
-	def rig_cards(player1_cards, player2_cards)
-		@players[0].set_cards(player1_cards)
-		@players[1].set_cards(player2_cards)
-	end
-end
-
-class WarPlayer
-	def set_cards(cards)
-		@cards = cards
-	end
-end
-
 server = WarGameServer.new
 server.accept_new_client
 server.accept_new_client
-
-hand1 = [PlayingCard.new('A', 'S'), PlayingCard.new('3', 'S')]
-hand2 = [PlayingCard.new('2', 'C'), PlayingCard.new('3', 'C')]
-server.game.rig_cards(hand1, hand2)
 
 loop do
 	server.take_commands
